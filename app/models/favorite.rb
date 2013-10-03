b25dadc64c2b9654cd0e27d859c55707266f4f20
Alan Carl Brown <syracal@gmail.com>
2013-10-03 07:03:27 -0700

    Added an association.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,7 @@ class Favorite < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
 
   belongs_to :user
+  belongs_to :cut
 
   validates :user_id, :presence => true
   validates :cut_id, :presence => true
